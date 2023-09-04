e6b44e76b3883aa38bee6cc5cf14023545d1faba
Madfater <ericleeddd@gmail.com>
2023-09-04 22:34:49 +0800

    feat:add local config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'
 import { UserModule } from './user/user.module';
 import { GoogleMapService } from './google-map/google-map.service';
 import { GoogleMapModule } from './google-map/google-map.module';
@@ -6,7 +7,7 @@ import { GoogleMapModule } from './google-map/google-map.module';
 
 
 @Module({
-  imports: [UserModule, GoogleMapModule],
+  imports: [UserModule, GoogleMapModule,ConfigModule.forRoot({isGlobal:true})],
   providers: [GoogleMapService],
 })
 export class AppModule {}
